b1cff0339e44bef02e5f560cc42681819350d617
Karthik <karthik5112001@gmail.com>
2022-09-08 20:49:36 +0530

    Js intro

---
+++ Portfolio/index.js
@@ -0,0 +1,44 @@
+//pop up box or dialog box
+
+//alert("Hello");
+//confirm("Are you leave This page");
+//prompt("Enter your value");
+
+ 
+if(true){
+    let b = 20;
+    b=1000;
+    console.log("This is block scope "+ b);
+}
+ 
+// integer
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+
+// array
+
+// object
+
+
+var a=10; //integer
+var b="10"; // string
+
+console.log(a==b);
+ 
+undefined
+var a=10; //integer
+var b="10"; // string
+
+console.log(a===b);
+
+// named function
+function myFunc()
+{
+    let a = 100;
+    console.log("This is function "+ a);
+}
+myFunc();
+ 
